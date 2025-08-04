9bc4c1554f4834a320b8ed5c064355f0eeb2408f
CalebEng <121829627+CalebEng@users.noreply.github.com>
2025-08-04 14:43:40 -0400

    added home page time

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
+let timeInterval;
 function load(page){
     fetch(`../views/${page}.html`)
     .then(response => response.text())
     .then(html => {
+        if(timeInterval){
+            clearInterval(timeInterval);
+        }
+
         document.getElementById('content').innerHTML = html;
+        if(page ==="home"){
+            displayTime();
+            timeInterval = setInterval(displayTime, 1000)
+        }
     })
     .catch(()=> {
         document.getElementById('content').innerHTML ="<p>Page not found.</p>";
     });
+}
+
+function displayTime(){
+   const timeElement = document.getElementById('time');
+  if (timeElement) {
+    const rawTime = new Date();
+    timeElement.textContent = rawTime.toLocaleTimeString();
+  }
 }
